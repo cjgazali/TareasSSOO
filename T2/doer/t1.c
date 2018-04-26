3acb74814c9093fbe7172f2e8bea99b1c56a6240
juan <jisilvamc>
2018-04-26 17:33:34 -0300

    asignado a struct

--- T2/doer/t1.c
+++ T2/doer/t1.c
@@ -23,44 +23,25 @@ tarea* crear_tarea(char* linea) {
 	int count = 0;
 	tarea* task = malloc(sizeof(tarea));
 	token = strtok(copia, " ");
-	task -> cmd = token;
+	task -> cmd = malloc(sizeof(char*));
+	strcpy(task -> cmd, token);
 
 	while (token != NULL){
-		//printf("%s %d \n", token, count);
-		//task -> argv = calloc(count+1, sizeof(char));
-		//task -> argv[count] = token;
-		//printf("%s", task -> argv[count]);
 		token = strtok(NULL, " ");
-		//printf("%s", task -> argv[count]);
 		count ++;
 	}
-	//printf("%d \n", count);
-	task -> argv = malloc(sizeof(char*)*(count-1));
-	task -> num_args = count-1;
-	//printf("%s", linea);
-
-	// asignar al array
+	task -> argv = malloc(sizeof(char**)*(count));
+	task -> num_args = count+1; 
 	count = 0;
 	token = strtok(linea, " ");
 	while (token != NULL){
-		//printf("%s \n", token);
-		task -> argv[count] = token;
+		task -> argv[count] = malloc(sizeof(char*));
+		strcpy(task -> argv[count], token);
 		token = strtok(NULL, " ");
-		//printf("%s", token);
 		count ++;
 	}
 
     task -> intentos = 0;
-    //printf("Comando: %s\n", task -> cmd);
-    //printf("Argumentos: \n");
-    //int i;
-    //int length;
-    //length = sizeof(task->argv) / sizeof(task->argv[0]);
-    //printf("Largo %d\n", count);
-    //printf("Comandos: \n");
-    //for (i=0; i <= count; i++){
-    //	printf("%s \n", task -> argv[i]);
-    //}
     return task;
 }
 
@@ -98,20 +79,10 @@ int main(int argc, char const *argv[])
     ssize_t read2;
     fp = fopen(filename, "r");
     while ((read2 = getline(&line, &len, fp)) != -1) {
-    	//lista = calloc(contador, sizeof(tarea));
-    	printf("READ %s %d\n", line, contador);
-        lista[contador] = crear_tarea(line);
-        //execvp(lista[contador]->cmd, lista[contador]->argv);
-        //printf("%s uf %s\n", crear_tarea(line)->cmd, lista[contador]->cmd);
-        
-        // OJO DE ACÁ EN ADELANTE
-        printf("Comando: %s\n", lista[0] -> cmd);
-        printf("%s \n", lista[0] -> argv[1]);
-        printf("%s \n", lista[contador] -> argv[1]);
-        printf("%d\n", contador);
-        //printf("\n");
+    	printf("%d ", contador);
+    	printf("READ %s", line);
+        lista[contador] = crear_tarea(line); 
         contador++;
-        // después de cada vuelta se cambian los atributos de todas las listas...
     } 
     fclose(fp);
     if (line){
@@ -120,23 +91,40 @@ int main(int argc, char const *argv[])
 
     int i = 0;
     int j = 0;
-    printf("Todo bien??\n");
+    printf("\n");
     for (i=0; i<contador; i++){
+    	printf("Comando: %s\n", lista[i]->cmd);
     	printf("Palabras: ");
     	for (j=0; j<(lista[i]->num_args); j++){
-    		printf("%s ", lista[i]->argv[j]);
+    		printf("%d: %s, ", j, lista[i]->argv[j]);
     	}
     	printf("\n");
     }
 
-    //char *cmd = "ls";
-    //char *arg[3];
-    //arg[0] = "ls";
-    //arg[1] = "-a";
-    //arg[2] = NULL;
-    //execvp(cmd, arg);
-
-
+    char* arg0[3];
+    arg0[0] = "ls";
+    arg0[1] = "-al";
+    arg0[2] = NULL;
+    //execvp(lista[0]->cmd, arg0);
+    printf("\n Empezamos a ejecutar");
+    int aux=0;
+    for (i=0; i<contador; i++){
+    	int pid = fork();
+    	int status;
+    	if (pid == 0){
+    		//execvp(lista[i]->cmd, lista[i]->argv);
+    		execvp(lista[0]->cmd, arg0);
+    	}
+    	if ((pid = waitpid(pid, &status, 1)) == 0) {
+			printf("Still running!\n");
+			sleep(1);
+		} 
+		else {
+			printf("Exit with code %d\n", status);
+		}
+    }
+    
+    //execvp(lista[0]->cmd, arg0);
     //endfree();
 
     return 0;
