3faf3e9ef410bf2e3fe9e2af2b7ceec7493a568c
Cristóbal Gazali <cjgazali@uc.cl>
2018-05-20 21:35:50 -0400

    mv y ls

--- T3/cz_API.c
+++ T3/cz_API.c
@@ -1,4 +1,10 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
+
+int i;
+int z;
 
 typedef struct dirBlockEntry {
 	unsigned char valid;
@@ -17,14 +23,48 @@ typedef struct dirBlockEntry {
 // 	db -> length = 64;
 // }
 
-direntry** dir_block = calloc(64, sizeof(direntry*))
+direntry** dir_block;  // = calloc(64, sizeof(direntry*));
 
 int cz_exists(char* filename) {
 	for (i = 0; i < 64; i++) {
-		entry_name = dir_block[i] -> name;
-		if (!strcmp(filename, entry_name)) {
+		if (!strcmp(filename, dir_block[i] -> name)) {
 			return 1;
 		}
 	}
 	return 0;
+}
+
+void save_name(char* old, char *dest) {
+	for (z = 0; z < strlen(dest); z++) {
+		old[z] = dest[z];
+		if (z == 9) {  // recorta si se excede
+			break;
+		}
+	}
+	old[z + 1] = '\0';
+}
+
+int cz_mv(char* orig, char *dest) {
+	for (i = 0; i < 64; i++) {
+		if (!strcmp(dest, dir_block[i] -> name)) {
+			return 1;
+		}
+	}
+	for (i = 0; i < 64; i++) {
+		if (!strcmp(orig, dir_block[i] -> name)) {
+			save_name(dir_block[i] -> name, dest);
+			break;
+		}
+	}
+	return 0;
+}
+
+
+void cs_ls() {
+	for (i = 0; i < 64; i++) {
+		direntry* entry = dir_block[i];
+		if (entry -> valid) {
+			printf("%s\n", entry -> name);
+		}
+	}
 }
--- T3/test.c
+++ T3/test.c
@@ -1,9 +1,14 @@
 #include <stdio.h>
 #include <string.h>
 
+
 int main(int argc, char const *argv[])
 {
-	char c[11] = "hola\0";
+	char c[11] = "hola";
+	int i;
+	for (i = 0; i < strlen(c); i++) {
+		printf("i %d\n", i);
+	}
 	printf("%s\n", c);
 	printf("%d\n", strcmp(c, "hola"));
 	printf("%d\n", strcmp(c, "hola\0"));
